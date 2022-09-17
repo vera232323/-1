c386e67feb72da160b515221acf3a48723c124b9
Vera <imbesom@mail.ru>
2022-09-17 20:05:00 +0300

    g

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine(a + " " + "= max," + b +" " + "= min");
+}
+else if(a<b)
+{
+    Console.WriteLine(b + " "+ "= max," + a + " " + "= min");
+}
+else if(a==b)
+{
+    Console.WriteLine(a + " "+ "=" +" " + b);
+}
